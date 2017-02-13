e3577bd57b503d7db662067da085898a4040ba47
Gary Black <gar2000b@yahoo.com>
2017-02-13 16:18:16 +0000

    Updating the wording for Decorator

--- DesignPatterns/src/main/java/com/onlineinteract/decoratorpattern/DecoratorPatternDemo.java
+++ DesignPatterns/src/main/java/com/onlineinteract/decoratorpattern/DecoratorPatternDemo.java
@@ -8,7 +8,8 @@ package com.onlineinteract.decoratorpattern;
  * All subscriptions should conform to the same interface
  * (SatelliteTV). We need an abstract decorator class that implements
  * that interface and holds a reference field of that interface too.
- * This field is used for wrapping / delegating calls up the stack.
+ * This field is used for wrapping / delegating calls down the call
+ * stack.
  * 
  * In our demo, we chain all the SatelliteTV constructors together for
  * all the subscriptions required. We of course specify our class to
